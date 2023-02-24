295e54f8263c3bbf71351d0e269516dd9fcdfb49
Philipp Roshchin <96698288+PHILIPP111007@users.noreply.github.com>
2023-02-25 02:02:59 +0300

    Update views.py

--- user/views.py
+++ user/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from .models import MyUser, Blog, Subscriber
-import re
-
 
 
 def index(request, username):
