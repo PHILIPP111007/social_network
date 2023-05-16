46fc8cafadd1b55aafa35ebf3cd65f575ea0240c
PHILIPP111007 <r.phil@yandex.ru>
2023-05-16 23:15:07 +0300

    bugfix

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.static import serve
+from .settings import STATICFILES_DIRS  # for debug = False
 
 
 urlpatterns = [
-	path('admin/', admin.site.urls),	
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': STATICFILES_DIRS[0]}),
+	path('admin/', admin.site.urls),
 	path('', include('register.urls'), name='home'),
 	path('user/<str:username>/', include('user.urls')),
 	path('friends/<str:username>/', include('friends.urls')),
