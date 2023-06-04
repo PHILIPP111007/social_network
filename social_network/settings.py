db900d8479378927470c2f6ec93e2562dd8a9a54
PHILIPP111007 <r.phil@yandex.ru>
2023-06-04 17:22:29 +0300

    bugfix

--- social_network/settings.py
+++ social_network/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import os
-from os import environ
+from os import getenv
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = int(environ.get('DEBUG', default=0))
+DEBUG = bool(getenv('DEBUG', default=0))
 
-ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = [getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -122,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'social_network/static')
+STATICFILES_DIRS = [
+	BASE_DIR / "social_network/static"
+]
 
 
 # Default primary key field type
@@ -133,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'social_network/static')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-LOGIN_URL = "/login"  # !!!
+LOGIN_URL = "/login"
 
 
 # ----- My settings -----
--- social_network/urls.py
+++ social_network/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.static import serve
+from .settings import STATICFILES_DIRS  # for debug = False
 
 
 urlpatterns = [
+    re_path(r"^static/(?P<path>.*)$", serve,{"document_root": STATICFILES_DIRS[0]}),
 	path("admin/", admin.site.urls),
 	path("", include("register.urls")),
 	path("user/<str:username>/", include("user.urls")),
